c79a3f26bbb8f5b4c8e8dadf9ad59ec16feafbf3
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2018-08-22 12:31:22 +0900

    build.sh作成

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -Ceu
+cd `dirname $0`
+
+TARGET_DIR="./koshian_del"
+OUTPUT_DIR=".."
+
+script_dir=$(cd $(dirname $0); pwd)
+addon_name=${script_dir##*/}
+addon_ver=$(jq ".version" ${TARGET_DIR}/manifest.json)
+filename="${OUTPUT_DIR}/${addon_name}-${addon_ver:1:-1}.zip"
+
+cd ${TARGET_DIR}
+zip -q -r -9 ${filename} * -x "*.bak" ".eslint*"
